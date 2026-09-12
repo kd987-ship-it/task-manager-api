dd6c65d4e94665e580ddba063d2d4cf0aa0c3999
kuldip8899 <savaliyakuldip8899@gmail.com>
2026-09-12 16:04:41 -0700

    Add endpoint to retrive task

--- app.py
+++ app.py
@@ -3,9 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn Git",
+        "completed": False,
+    },
+    {
+        "id": 2,
+        "title": "Create first pull request",
+        "completed": False,
+    },
+]
+
+
 @app.route("/")
 def home():
     return {"message": "Task Manager API is running"}
+
+
+@app.route("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
 
 
 if __name__ == "__main__":
