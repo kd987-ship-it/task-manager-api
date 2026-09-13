7967a10c877fe6055202edc58a0fe35734fa1880
kuldip8899 <savaliyakuldip8899@gmail.com>
2026-09-12 17:03:40 -0700

    Add endpoint to create tasks

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 
-from services.task_service import get_all_tasks
+from services.task_service import get_all_tasks, create_task
 
 app = Flask(__name__)
 
@@ -10,9 +10,18 @@ def home():
     return {"message": "Task Manager API is running"}
 
 
-@app.route("/tasks")
+@app.route("/tasks", methods=["GET"])
 def get_tasks():
     return {"tasks": get_all_tasks()}
+
+
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    data = request.get_json()
+
+    task = create_task(data["title"])
+
+    return task, 201
 
 
 if __name__ == "__main__":
--- services/task_service.py
+++ services/task_service.py
@@ -14,3 +14,15 @@ tasks = [
 
 def get_all_tasks():
     return tasks
+
+
+def create_task(title):
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": title,
+        "completed": False
+    }
+
+    tasks.append(new_task)
+
+    return new_task
