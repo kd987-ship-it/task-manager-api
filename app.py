cfad2c535aba1af9340568c3afa64fe8718a8db8
kd987-ship-it <kuldipr.987@gmail.com>
2026-09-12 17:33:37 -0700

    Feature/task 102 create task (#2)
    * Add endpoint to create tasks
    
    * Update API home message
    
    ---------
    
    Co-authored-by: kuldip8899 <savaliyakuldip8899@gmail.com>

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
-from services.task_service import get_all_tasks
+from services.task_service import get_all_tasks, create_task
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return {"message": "Task Management Service is running"}
+    return {"message": "Task Manager API v2 is running"}
 
 
-@app.route("/tasks")
+@app.route("/tasks", methods=["GET"])
 def get_tasks():
     return {"tasks": get_all_tasks()}
+
+
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    data = request.get_json()
+
+    task = create_task(data["title"])
+
+    return task, 201
 
 
 if __name__ == "__main__":
